7bad4d1313cbfe9f7e014781b10aabf6d6a3f986
kiran kumar R <kiranidentity@gmail.com>
2026-02-01 13:38:54 +0530

    UI Polish: Separated Global AppIdentity from Contextual Page Titles

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "523485081"
+    serviceWorkerVersion: "3747382994"
   }
 });
